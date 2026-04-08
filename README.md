22f426d9b3708f02739583e0c05e360447c55bb3
ayushgi <gitayush30@gmail.com>
2026-04-08 23:36:26 +0530

    apply all the frontend part

--- README.md
+++ README.md
@@ -2,3 +2,6 @@ __CONTENT BASED MOVIE RECOMENDATION SYSTEM__
 
 This project is a Content-Based Movie Recommendation System that suggests movies similar to a given movie based on its features such as:
 genres, keywords, cast, and overview.
+
+
+API for tmdb=b18404e6a4d424ddf83d06ef9e369821
--- app.py
+++ app.py
@@ -3,10 +3,23 @@ import pickle
 import pandas as pd
 import os
 import gdown
+import requests
 #similarity.pkl download
 if not os.path.exists("similarity.pkl"):
     url = "https://drive.google.com/uc?id=18Mtt68SKl7UDFIV-kz_UwQrzfVD5pCQ8"
     gdown.download(url, "similarity.pkl", quiet=False)
+def fetch_poster(movie_id):
+    response = requests.get(
+        'https://api.themoviedb.org/3/movie/{}?api_key=b18404e6a4d424ddf83d06ef9e369821&language=en-US'.format(movie_id)
+    )
+    data = response.json()
+
+    poster_path = data.get('poster_path')
+
+    if poster_path:
+        return "https://image.tmdb.org/t/p/w500/" + poster_path
+    else:
+        return "https://via.placeholder.com/500x750?text=No+Image"
 def recommend(movie):
 
     movie_index=movies[movies['title']==movie].index[0]
@@ -15,9 +28,13 @@ def recommend(movie):
 
 
     recommend_movies=[]
+    recommend_movies_poster=[]
     for i in movie_list:
+        movie_id = movies.iloc[i[0]]['id'] 
+        #fetch poster from api
         recommend_movies.append(movies.iloc[i[0]].title)
-    return recommend_movies
+        recommend_movies_poster.append(fetch_poster(movie_id))
+    return recommend_movies, recommend_movies_poster
 
 
 movies = pickle.load(open('movies.pkl', 'rb'))
@@ -31,7 +48,12 @@ similarity = pickle.load(open('similarity.pkl', 'rb'))
 st.title('Movie Recommender System')
 selected_movie_name = st.selectbox("Select  a movie from here", movies_list)
 if st.button('Recommend'):
-    recommendation=recommend(selected_movie_name)
-    for i in recommendation:
-        st.write(i)
-#this is all through 
+    names, poster = recommend(selected_movie_name)
+    cols=st.columns(5)
+    for i in range(5):
+        with cols[i]:
+            st.write(names[i])
+            st.image(poster[i])
+
+
+
